c9c3d85da85bffa3d5f8f48ebf5175ab4e31e400
Jonathan Marshall <jwmarsh16@gmail.com>
2024-06-20 12:49:45 -0500

    added style to AddSong

--- src/components/AddSong.jsx
+++ src/components/AddSong.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import { baseUrl } from "../globals";
+import '../Style/AddSong.css'
 
 function AddSong({addNewSong}) {
 
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -5,7 +5,7 @@ function Home() {
     return (
         <div className="home-container">
             <h1>Music Playlist Manager</h1>
-            <p>Welcome to the ultimate music playlist manager. Organize your music with ease.</p>
+            <p>Welcome to the music playlist manager. Organize your music with ease.</p>
         </div>
     );
 }
